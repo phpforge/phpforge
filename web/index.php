4731a96c41d010064b48be4886779ffc866b03de
Benjamin C. Tehan <benjamin.tehan@devforge.io>
2016-11-19 20:01:50 +1100

    Update structure

--- web/index.php
+++ web/index.php
@@ -3,8 +3,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+chdir(dirname(__FILE__));
+
 DEFINE('DS', DIRECTORY_SEPARATOR);
-DEFINE('BASE_URI', preg_replace('/\/index.php$/', '', $_SERVER['PHP_SELF']));
+
+
+DEFINE('BASE_URI', preg_replace(
+	array('/\/$/', '/\/sssweb$/'),
+	'',
+	key_exists('REDIRECT_BASE_URL', $_SERVER) ? filter_input(INPUT_SERVER, 'REDIRECT_BASE_URL') : filter_input(INPUT_SERVER, 'BASE_URL'))
+);
 
 use Forge\Application;
 
